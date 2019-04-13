fabb737a8abb44d7950083e57556b5789fa86eb2
Jacob Hardin-Bernhardt <jAKEHB2@gmail.com>
2019-04-12 21:20:52 -0400

    Some stuff idk

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get '/converter/temperature/convert/:tStart/:d1/to/:d2', to: 'converter#temperature'
+<<<<<<< HEAD
+=======
+  root 'welcome#index'
+>>>>>>> 449b04c3933c420ac84a4e8c538472e6ee670996
 end
